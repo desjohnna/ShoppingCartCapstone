1c8b991e4deb85ac1b4c208d8ce5fd515e11edb4
DesJohnna <desjohnna@gmail.com>
2019-09-05 17:56:57 -0500

    first commit

---
+++ ShoppingCartCapstone/src/main/java/com/company/ShoppingCartCapstone/ShoppingCartCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.ShoppingCartCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartCapstoneApplication.class, args);
+	}
+
+}
